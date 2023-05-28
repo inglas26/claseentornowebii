537f1f84b3351a0bbca47972ac8e16bcbe435405
inglas26 <134826482+inglas26@users.noreply.github.com>
2023-05-28 03:21:42 -0500

    Ingreso de datos y texto Javascript

---
+++ laboratorio/apps.js
@@ -0,0 +1,11 @@
+// Ingresar nombres
+let nombrePersona = prompt("Por favor ingrese su nombre: ");
+
+// Ingresar apellido paterno
+let apellidoPaterno = prompt("Por favor ingrese su apellido paterno: ");
+
+//Ingresar apellido materno
+let apellidoMaterno = prompt("Por favor ingrese su apellido materno: ");
+
+// Mostrar en cuadro emergente los datos ingresados
+alert("Hola " + nombrePersona + " " + apellidoPaterno + " " + apellidoMaterno + " haz ingresado correctamente");
