8d05ab9f02f16a43e0d6b78f72aa7e1ce0bf8b9f
inglas26 <134826482+inglas26@users.noreply.github.com>
2023-05-28 03:55:38 -0500

    cargando datos en la web utilizando javascript

---
+++ laboratorio/laboratorio4/js/mensajes.js
@@ -0,0 +1,21 @@
+// Pedimos los datos a través del prompt
+let nombrePersona = prompt("Ingresa tu nombre: ");
+
+document.write("<h1>Bienvenido a Cibertec</h1>");
+
+// Visualizamos lo ingresado en la página web
+document.write('<h3>' + "Nombre: " + nombrePersona + '</h3>');
+
+// Pedimos los datos a través del prompt
+let nombrePersona = prompt("Ingresa tu nombre: ");
+
+document.write("<h1>Bienvenido a Cibertec</h1>");
+
+// Visualizamos lo ingresado en la página web
+document.write('<h3>' + "Nombre: " + nombrePersona + '</h3>');
+
+//Visualice los apellidos
+document.write("<h3>" + "Apellido: " +  apellidoPersona + "</h3");
+
+//Visualice nombre del curso
+document.write("<h3>" + "Curso: " +  nombreCurso + "</h3");
