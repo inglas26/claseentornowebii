76704f8b6238877029abb636a5488923ccf90b8c
inglas26 <134826482+inglas26@users.noreply.github.com>
2023-05-28 04:46:24 -0500

    Se trabaja con texto en javascript

---
+++ laboratorio/laboratorio8/js/textos.js
@@ -0,0 +1,11 @@
+let contenido = document.body.querySelector('input#mensaje');
+
+let cargarTexto = document.body.querySelector ("#cargar-texto");
+cargarTexto.addEventListener('click', () => {
+      contenido.value = "Vengo desde Javascript";
+});
+
+let limpiarTexto = document.body.querySelector('#quitar-texto');
+limpiarTexto.addEventListener('click', () =>{
+       contenido.value = '';
+});
