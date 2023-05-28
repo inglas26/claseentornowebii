f9a26612886712b7dee48155246894624ba396bb
inglas26 <134826482+inglas26@users.noreply.github.com>
2023-05-28 03:49:24 -0500

    edad en javascript

---
+++ laboratorio/laboratorio3/js/edad.js
@@ -0,0 +1,9 @@
+let nombrePersona = prompt("Ingrese su nombre:");
+
+let anioNacimiento = parseInt(prompt("Ingrese su año de nacimiento:"));
+
+const anioActual = 2023;
+
+let edad = anioActual - anioNacimiento;
+
+alert("Hola " + nombrePersona + ", tienes " + edad + " años.");
